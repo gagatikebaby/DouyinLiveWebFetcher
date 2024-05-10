81cdb833afcd5d7aef48399c720409254c3546c6
a407259222@163.com <407259222@qq.com>
2024-05-10 11:47:21 +0800

    test

--- main.py
+++ main.py
@@ -6,7 +6,17 @@
 # @Author:      bubu
 # @Project:     douyinLiveWebFetcher
 
+import asyncio
+import websockets
 from liveMan import DouyinLiveWebFetcher
+
+# async def echo(websocket, path):
+#     async for message in websocket:
+#         print(f"Received message: {message}")
+#         await websocket.send(f"Echo: {message}")
+# start_server = websockets.serve(echo, "localhost", 8765)
+# asyncio.get_event_loop().run_until_complete(start_server)
+# asyncio.get_event_loop().run_forever()
 
 if __name__ == '__main__':
     live_id = '26012747113'
